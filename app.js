13d58cc5f9ce9ea1c7236e94bdba6af1a8c9cfaf
SkylerCrangle <smcrangle@gmail.com>
2020-01-27 15:24:52 -0700

    made a big practice object with nested objects

---
+++ app.js
@@ -0,0 +1,28 @@
+let sentient = {
+  myName: "Skyler",
+  humanoid: true,
+  human: false,
+  age: 535,
+  homePlanet: "Saturn",
+  northPole: "Hexagon",
+  ears: "pointed",
+  favoriteFlower: "Bleeding Heart",
+  cupOfTea: ["Liu Bao", "Wild Purple White", "15 years aged Green"],
+  stone: "labradorite",
+  pet: {
+    petName: "Titan",
+    petType: "Cat",
+    likesDogs: false,
+    likesWater: true,
+    likesRain: false,
+    wantsToPlay: true,
+    likesKids: true,
+    size: "small",
+    appetite: "large",
+    toy: {
+      whatIsIt: "a string",
+      onTheCouch: "NEVER",
+      runAroundWhileBeingChased: "Perfection",
+    }
+  }
+}
